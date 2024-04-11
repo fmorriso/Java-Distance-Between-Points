a088fa41eb7449e42736e898f723d2b0fb32d67f
Frederick Morrison <frederick.morrison@outlook.com>
2024-04-11 11:04:22 -0600

    add display of java version and date/time

---
+++ Main.java
@@ -0,0 +1,31 @@
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        System.out.format("Distance between points using java version %s on %s%n",
+                getJavaVersion(), getCurrentLocalDateTime());
+    }
+
+    /** get the java version that is running the current program
+     * @return string containing the java version running the current program
+     */
+    private static String getJavaVersion()
+    {
+        Runtime.Version runTimeVersion = Runtime.version();
+        return String.format("%s.%s.%s.%s", runTimeVersion.feature(), runTimeVersion.interim(), runTimeVersion.update(), runTimeVersion.patch());
+    }
+
+
+    /** Get the local date and time in a format similar to ISO 8601 format
+     * @return string containing the current local Date/Time in a format close to ISO 8601 format (yyyy-mm-dd hh:mm:ss)
+     */
+    private static String getCurrentLocalDateTime()
+    {
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        return String.format(dtf.format(LocalDateTime.now()));
+    }
+
+}
